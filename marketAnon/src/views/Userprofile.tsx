d3f7bb49cd913edc96d36450812e13286e2874dc
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-25 01:34:49 -0500

    user profile page layout setup & css

--- marketAnon/src/views/Userprofile.tsx
+++ marketAnon/src/views/Userprofile.tsx
@@ -1,14 +1,40 @@
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
-
-
+//bring in current user to display username
+//display THEIR reviews
 type UserprofileProps = {}
 
 export default function Userprofile({}: UserprofileProps) {
   return (
-    <div>
-      <h1>
-        Userprofile
-      </h1>
-      </div>
+    <>
+    <Row>
+      <Col className='mt-5'>
+      <h1 className='text-pink'>Username</h1>
+      </Col>
+    </Row>
+    <Row>
+      <img></img>
+      <h1>Profile Pic</h1>
+    </Row>
+    <Row className='mt-4'>
+        <Card bg='dark' text='white'>
+              <Card.Header>
+                <h3>User's Name</h3>
+                <Card.Subtitle> User's Company & Job Title</Card.Subtitle>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <p>User's Review 1</p>
+                  <p>User's Review 2</p>
+                  <p>User's Review 3</p>
+                  <p>User's Review 4</p>
+                </Card.Text>
+              </Card.Body>
+        </Card>
+    </Row>
+
+    </>
   )
 }
