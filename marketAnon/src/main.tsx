964761d60f31f5782f9dfac0f32fa8db6800355f
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-05-15 18:18:05 -0500

    amplify config

--- marketAnon/src/main.tsx
+++ marketAnon/src/main.tsx
@@ -1,3 +1,6 @@
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '/Users/alexandriayoung/Documents/CodingTemple/CAPSTONE/SRC/react_marketanon/src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
